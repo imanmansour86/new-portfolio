24ebdf7a2c1dfead26124fcb6b9a1d583ccf8e3f
imanmansour86 <imanmansour86@gmail.com>
2022-01-20 15:27:44 -0800

    fixing glide arrow for small devices layout

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-new Glide(".glide").mount();
+new Glide(".glide").mount(); //mount the glide
 new Glide(".glide", {
   type: "carousel",
   startAt: 1,
@@ -6,12 +6,15 @@ new Glide(".glide", {
   perView: 3,
 });
 
-let first = $(".first");
-let singleObject;
+//store reference for each li in glide
+let firstRepo = $(".first");
+let secondRepo = $(".second");
+let thirdRepo = $(".third");
+let fourthRepo = $(".fourth");
 
-// read all repos from git
+// read all starred repos from git
 const allRepos = () => {
-  var apiUrl = " https://api.github.com/users/imanmansour86/starred"; //get starred repos
+  var apiUrl = " https://api.github.com/users/imanmansour86/starred";
 
   fetch(apiUrl)
     .then(function (response) {
@@ -21,13 +24,22 @@ const allRepos = () => {
       getReadMe(data);
     });
 };
-let newElement;
-function getReadMe(result) {
+
+const getReadMe = (result) => {
   console.log("results is", result);
+  // for each starred repo get the repo description
   for (i in result) {
-    newElement = `<div><p>${result[3].description}<p><div>`;
+    firstElement = `<div class="details"><p>${result[3].description}<p><div>`;
+    secondElement = `<div class="details"><p>${result[2].description}<p><div>`;
+    thirdElement = `<div class="details" ><p>${result[0].description}<p><div>`;
+    fourthElement = `<div class="details"><p>${result[1].description}<p><div>`;
   }
-  first.append(newElement);
-}
+
+  //append description to each lit
+  firstRepo.append(firstElement);
+  secondRepo.append(secondElement);
+  thirdRepo.append(thirdElement);
+  fourthRepo.append(fourthElement);
+};
 
 allRepos();
