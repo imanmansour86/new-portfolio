8c936ae3d45ee026e06a9972e516ba7f23cd601c
imanmansour86 <imanmansour86@gmail.com>
2022-01-20 08:44:03 -0800

    reading repo description from api

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-console.log("test");
 new Glide(".glide").mount();
 new Glide(".glide", {
   type: "carousel",
@@ -6,3 +5,29 @@ new Glide(".glide", {
   gap: 0,
   perView: 3,
 });
+
+let first = $(".first");
+let singleObject;
+
+// read all repos from git
+const allRepos = () => {
+  var apiUrl = " https://api.github.com/users/imanmansour86/starred"; //get starred repos
+
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      getReadMe(data);
+    });
+};
+let newElement;
+function getReadMe(result) {
+  console.log("results is", result);
+  for (i in result) {
+    newElement = `<div><p>${result[3].description}<p><div>`;
+  }
+  first.append(newElement);
+}
+
+allRepos();
