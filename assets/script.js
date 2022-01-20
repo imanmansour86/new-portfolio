2cce3c7f74a2cfda449b203f389babda235d1fa5
imanmansour86 <imanmansour86@gmail.com>
2022-01-20 00:01:19 -0800

    adding media query for hero text on small devices

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-// console.log("test");
-
-// new Glide(".glide", {
-//   type: "carousel",
-//   startAt: 0,
-//   perView: 3,
-// });
+console.log("test");
+new Glide(".glide").mount();
+new Glide(".glide", {
+  type: "carousel",
+  startAt: 0,
+  perView: 3,
+});
